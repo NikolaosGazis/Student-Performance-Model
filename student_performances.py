74beea142fc7d0d39d71290c80b195560b19aae9
Nick Gazis <nikolaosgazis3008@gmail.com>
2024-08-18 03:27:20 +0300

    Refactored Code.

--- student_performances.py
+++ student_performances.py
@@ -1,12 +1,10 @@
-##### Νικόλαος Νεκτάριος Γαζής - Α.Μ.: 212123(1) - Python 3.11.5 #####
-##### webmail: nigazis@uth.gr #####
 ##### Dataset Link: https://www.kaggle.com/datasets/spscientist/students-performance-in-exams #####
 
 # Libraries #
 import pandas as pd
+import matplotlib.pyplot as plot
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plot
 
 # Variables #
 print_students = 10
@@ -42,13 +40,13 @@ def numeric_conversion(data):
 def intervals(): 
     while True:
         try:
-            num_intervals = int(input("How many Intervals you wish to have? -> "))
+            num_intervals = int(input("[SYSTEM] How many Intervals do you wish to have? -> "))
             if 1 <= num_intervals <= 100: # Positive.
                 return num_intervals # Successful.
             else:
-                print("\tThe input was out of bounds, please renter your input.\n")
+                print("[SYSTEM] The input was out of bounds, please renter your input. -> ")
         except ValueError as e:
-            print(f"Input was invalid. Log: {e}")
+            print(f"Input was invalid, enter an integer. Log: {e}")
 
 # Enter the data elements for a new student #
 def add_student():
@@ -57,80 +55,80 @@ def add_student():
     # Inputs - dictionaries #
     # gedner #
     while True:
-        gender_in = input('•Enter gender -> ').lower()
+        gender_in = input('[SYSTEM] Enter gender -> ').lower()
         if gender_in in ['male', 'female']:
             student_data['gender'] = gender_in
             break
         else:
-            print("\tInvalid input for the gender, try again.\n")
+            print("[SYSTEM] Invalid input for the gender attribute, try again.\n")
             
     # race #
     while True:
-        race_in = input('•Enter race/ethnicity (Only the letter) -> ').upper()
+        race_in = input('[SYSTEM] Enter race/ethnicity (Only the letter) -> ').upper()
         if race_in in ['A', 'B', 'C', 'D', 'E']:
             student_data['race/ethnicity'] = "group " + race_in
             break
         else:
-            print("\tInvalid input for the race, try again.\n")
+            print("[SYSTEM] Invalid input for the race attribute, try again.\n")
     
     # parental level of education #
     while True:
-        ple_in = input('•Enter parental level of education -> ').lower()
+        ple_in = input('[SYSTEM] Enter parental level of education -> ').lower()
         if ple_in in ['some high school', 'high school', 'some college', "associate's degree", "bachelor's degree", "master's degree"]:
             student_data['parental level of education'] = ple_in
             break
         else:
-            print("\tInvalid input for the parental level of education, try again.\n")
+            print("[SYSTEM] Invalid input for the parental level of education attribute, try again.\n")
     
     # lunch #
     while True:
-        lunch_in = input('•Enter lunch: ').lower()
+        lunch_in = input('[SYSTEM] Enter lunch: ').lower()
         if lunch_in in ['free/reduced', 'standard']:
             student_data['lunch'] = lunch_in
             break
         else:
-            print("\tInvalid input for the lunch, try again.\n")
+            print("[SYSTEM] Invalid input for the lunch attribute, try again.\n")
     
     # test preparation #  
     while True:
-        test_prep_in = input('•Enter test preperation course -> ').lower()
+        test_prep_in = input('[SYSTEM] Enter test preperation course -> ').lower()
         if test_prep_in in ['none', 'completed']:
             student_data['test preparation course'] = test_prep_in
             break
         else:
-            print("\tInvalid input for the test preparation course, try again.\n")
+            print("[SYSTEM] Invalid input for the test preparation course attribute, try again.\n")
     
     # scores #
-    while True:
+    while True: # math.
         try:
-            math_score_in = int(input('•Enter math score -> '))
+            math_score_in = int(input('[SYSTEM] Enter math score -> '))
             if 1 <= math_score_in <= 100:
                 student_data['math score'] = math_score_in
                 break
             else:
-                print("\tInvalid input, math score must be an integer and between 1 to 100")
+                print("[SYSTEM] Invalid input, math score must be an integer and between 1 to 100")
         except ValueError as e:
-            print(f"\tAn error came up. Log: {e}\n")
+            print(f"[SYSTEM] An error came up. Log: {e}\n")
             
-    while True:
+    while True: # reading.
         try:
-            reading_score_in = int(input('•Enter reading score -> '))
+            reading_score_in = int(input('[SYSTEM] Enter reading score -> '))
             if 1 <= reading_score_in <= 100:
                 student_data['reading score'] = reading_score_in
                 break
             else:
-                print("\tInvalid input, reading score must be an integer and between 1 to 100")
+                print("[SYSTEM] Invalid input, reading score must be an integer and between 1 to 100")
         except ValueError as e:
-            print(f"\tAn error came up. Log: {e}\n")
+            print(f"[SYSTEM] An error came up. Log: {e}\n")
 
-    while True:
+    while True: # writing.
         try:
-            writing_score_in = int(input('•Enter writing score -> '))
+            writing_score_in = int(input('[SYSTEM] Enter writing score -> '))
             if 1 <= writing_score_in <= 100:
                 student_data['writing score'] = writing_score_in
                 break
             else:
-                print("\tInvalid input, writing score must be an integer and between 1 to 100")            
+                print("[SYSTEM] Invalid input, writing score must be an integer and between 1 to 100")            
         except ValueError as e:
             print(f"\tAn error came up. Log: {e}\n")
             
@@ -138,7 +136,7 @@ def add_student():
 
 # Find the closest students to each addded one - data: original data #
 def close_students(new_data, data):
-    # Filter students for the new data - make a copy of the original DataFrame / avoid warnings #
+    # Filter students for the new data - make a copy of the original DataFrame #
     same_cluster = data[data['cluster'] == new_data['cluster'].values[0]].copy()
     same_cluster_cp = same_cluster[['math score', 'reading score','writing score']].copy()
     
@@ -172,17 +170,17 @@ def main():
     final_data['cluster'] = data_kmeans.fit_predict(scaled_data)
     
     # -> Display Data Statistics #
-    plot.scatter(final_data['average score'], final_data['cluster']+1, c=final_data['cluster'], cmap='magma') # x, y, color, variant of colors.
+    plot.scatter(final_data['average score'], final_data['cluster'] + 1, c=final_data['cluster'], cmap='viridis') # x, y, color, variant of colors.
     plot.title('Clusterism')
     plot.xlabel("Student's average scores")
-    plot.ylabel('Cluster:')
+    plot.ylabel('Cluster')
     plot.show() # display.
 
     # -> Ask the user is he wishes to input a new student #
     while True: 
         try:
-            answer = str(input("\nYou wish to add another stunent? yes/no -> "))
-            if answer.lower() == 'yes':
+            answear = str(input("\nDo you wish to add another stunent? yes/no -> ")).strip().lower()
+            if answear == 'yes':
                 new_student = add_student()
                 # Redo the process like before #
                 new_student = pd.DataFrame([new_student]) # dataframe.
@@ -192,15 +190,17 @@ def main():
                 new_student_final['cluster'] = data_kmeans.predict(new_student_scaled) # clusterize.
                 
                 # Print the results #
-                print(f"\n•Cluster in which the student belongs: {new_student_final['cluster'].values[0] + 1}")
+                print(f"\n[SYSTEM] Cluster in which the student belongs: {new_student_final['cluster'].values[0] + 1}")
                 # Find the print the closest students #
                 closest_students = close_students(new_student_final, final_data)
-                print(f"\nHere are the closest students: {closest_students}")
-            elif answer.lower() == 'no':
+                print(f"\n[SYSTEM] Here are the closest students: {closest_students}")
+            elif answear == 'no':
                 break
+            else:
+                print("[SYSTEM] Answear must be either 'yes' or 'no'.")
         except ValueError as e:
-            print(f"Invalid input. Log: {e}")
+            print(f"[SYSTEM] Invalid input. Log: {e}")
             
-# Run the program #
+# Execute the program #
 if __name__ == '__main__':
     main()
